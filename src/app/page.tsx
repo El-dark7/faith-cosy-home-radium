4cdd6d822f2380a874059e1c42fb3341e2455d72
El-dark7 <nobodynobadyknws@gmail.com>
2026-06-21 20:27:08 +0300

    Create HomePage component with Navbar, Hero, and Amenities

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Navbar from "@/components/Navbar";
+import Hero from "@/components/Hero";
+import Amenities from "@/components/Amenities";
+
+export default function HomePage() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Amenities />
+    </>
+  );
+}
