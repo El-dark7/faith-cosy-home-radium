ead58377329eb3eb05cebb286b781416a9d621da
El-dark7 <nobodynobadyknws@gmail.com>
2026-06-21 20:22:36 +0300

    Add constants for property details

---
+++ src/lib/constants.ts
@@ -0,0 +1,9 @@
+export const property = {
+  name: "Radium Residence",
+  company: "Faith Cosy Homes",
+  email: "Matufaith135@gmail.com",
+  phone1: "0793667999",
+  phone2: "0797623102",
+  whatsapp: "254793667999",
+  location: "Nyali, Mombasa",
+};
