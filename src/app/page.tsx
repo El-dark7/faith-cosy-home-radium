b12ccc8f9da06a7e37ee3872c63769f7f5cf102b
El-dark7 <nobodynobadyknws@gmail.com>
2026-06-21 21:13:10 +0300

    Add new components to HomePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,30 @@
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
 import Amenities from "@/components/Amenities";
+import PropertyShowcase from "@/components/PropertyShowcase";
+import Reviews from "@/components/Reviews";
+import GoogleMap from "@/components/GoogleMap";
+import Footer from "@/components/Footer";
+import WhatsAppFloat from "@/components/WhatsAppFloat";
 
 export default function HomePage() {
   return (
     <>
       <Navbar />
+
       <Hero />
+
       <Amenities />
+
+      <PropertyShowcase />
+
+      <Reviews />
+
+      <GoogleMap />
+
+      <Footer />
+
+      <WhatsAppFloat />
     </>
   );
 }
