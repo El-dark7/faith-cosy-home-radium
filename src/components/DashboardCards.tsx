bfc9867aa993910c5342033595f63daa330738e0
El-dark7 <nobodynobadyknws@gmail.com>
2026-06-23 16:23:06 +0300

    Add DashboardCards component for displaying metrics

---
+++ src/components/DashboardCards.tsx
@@ -0,0 +1,27 @@
+export default function DashboardCards(){
+
+ return(
+
+ <div className="grid md:grid-cols-4 gap-6">
+
+  <div className="p-6 rounded-xl shadow">
+   Total Bookings
+  </div>
+
+  <div className="p-6 rounded-xl shadow">
+   Guests
+  </div>
+
+  <div className="p-6 rounded-xl shadow">
+   Reviews
+  </div>
+
+  <div className="p-6 rounded-xl shadow">
+   Messages
+  </div>
+
+ </div>
+
+ );
+
+}
